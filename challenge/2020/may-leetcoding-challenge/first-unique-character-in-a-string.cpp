1b46ed9195de5e5208bc5bf1b3cdb9c3929ac7d2
Vasu Goel <goelvasu0712@gmail.com>
2021-01-04 16:51:09 +0530

    First Unique Character in a String

---
+++ challenge/2020/may-leetcoding-challenge/first-unique-character-in-a-string.cpp
@@ -0,0 +1,13 @@
+// O(n) time, O(1) space
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26);
+        for(char c: s)  freq[c-'a']++;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(freq[s[i]-'a'] == 1)    return i;
+        }
+        return -1;
+    }
+};
