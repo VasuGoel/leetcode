7c5f48c18f2ed24656069609c835b56068132f06
Vasu Goel <goelvasu0712@gmail.com>
2021-01-19 17:11:01 +0530

    Detect Capital

---
+++ challenge/2020/august-leetcoding-challenge/detect-capital.cpp
@@ -0,0 +1,11 @@
+// O(n) time, O(1) space. Single pass.
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        for(int i = 1; i < word.size(); i++)
+            if((islower(word[0]) and isupper(word[i])) or (islower(word[1]) ^ islower(word[i])))
+                return false;
+        
+        return true;
+    }
+};
