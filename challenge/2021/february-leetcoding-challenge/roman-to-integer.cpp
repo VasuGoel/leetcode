9135493b012483af50a364e67b87a70a95e73da8
Vasu Goel <goelvasu0712@gmail.com>
2021-02-20 18:05:39 +0530

    Roman to Integer

---
+++ challenge/2021/february-leetcoding-challenge/roman-to-integer.cpp
@@ -0,0 +1,16 @@
+// O(n) time, O(1) space
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        
+        int res = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(i and m[s[i]] > m[s[i-1]])
+                res -= 2 * m[s[i-1]];
+            
+            res += m[s[i]];
+        }
+        return res;
+    }
+};
