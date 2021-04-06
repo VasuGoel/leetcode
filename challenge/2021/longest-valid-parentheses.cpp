5101a058e9ed187bb4062adaf7bf4c7b8152e418
Vasu Goel <goelvasu0712@gmail.com>
2021-04-06 18:38:40 +0530

    Longest Valid Parentheses

---
+++ challenge/2021/longest-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size(), longest = 0;
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(')     st.push(i);
+            else {
+                st.pop();
+                if(st.empty())   st.push(i);
+                else    longest = max(longest, i-st.top());
+            }
+        }
+        return longest;
+    }
+};
